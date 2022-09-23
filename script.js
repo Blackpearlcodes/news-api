4c6826f8e940fea6446f00ee7adb44585ba9f2dc
Ghassan Fares <gfares@gmail.com>
2022-09-23 23:28:08 +0300

    js minor fixes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 const news = document.getElementById("news")
+
 $.get(
 "https://newsapi.org/v2/everything?q=Apple&from=2022-09-23&sortBy=popularity&apiKey=5c6eae8d52cc49ac8478bba16c34b657",
 (dataObj) => {
