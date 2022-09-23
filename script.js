15aac40da05142cda688c1fde652c9d701ec243c
Ghassan Fares <gfares@gmail.com>
2022-09-23 23:13:10 +0300

    js edit1

--- script.js
+++ script.js
@@ -5,37 +5,3 @@ $.get(
     dataObj.articles.forEach(data => {
         const card = document.createElement("div")
         card.className = 'cards-ch'
-
-        const title = document.createElement("h1")
-        title.className = 'title'
-        title.id = 'title'  
-        title.textContent = data.title
-        card.append(title)
-
-        const img = document.createElement("img")
-        img.src = data.urlToImage
-        img.style.maxWidth = '95%'
-        img.style.margin = '35px auto'
-        card.append(img)
-
-        const source = document.createElement("div")
-        source.className = 'source'
-        source.id = 'source'
-        source.textContent = data.source.name
-        card.append(source)
-
-        const content = document.createElement("div")
-        content.className = 'content'
-        content.id = 'content'
-        content.textContent = data.description
-        card.append(content)
-
-        const link = document.createElement('a')
-        link.className = 'linkToNews'
-        link.href = data.url
-        link.textContent = 'Read More'
-        card.append(link)
-        
-        news.append(card)
-    });
-})
